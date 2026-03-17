fcf95982de030d9c346ea5d8f0733f80c8637d32
naya <nayakhairunnisa24012011@gmail.com>
2026-03-18 00:03:05 +0700

    up

---
+++ sewabaju/resources/views/contact.blade.php
@@ -0,0 +1,3 @@
+<div>
+    Silakan menghubungi kontak yang tersedia untuk melakukan pemesanan.
+</div>
---
+++ sewabaju/resources/views/home.blade.php
@@ -0,0 +1,6 @@
+<div>
+<!-- Simplicity is an acquired taste. - Katharine Gerould --> 
+ Selamat Datang di Toko Sewa Baju Terbaik {{$nama}}<br>
+Anda adalah seorang {{$pekerjaan}}
+</div>
+
